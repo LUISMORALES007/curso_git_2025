3fed7c983e80842bb9881666a4d5bf2c699c4bc9
LuisMi VMWare. <Luismiguel920127@gmail.com>
2025-07-01 12:15:55 -0500

    agregar api superheroes marvel

--- API_Python/app.py
+++ API_Python/app.py
@@ -14,3 +14,8 @@ def get_familia():
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
+
+@app.get("/superheroesMarvel")
+def get_superheroes():
+    rows = ["Spiderman", "Thor", "Ironman", "Hulk", "Wolverine"]
+    return rows    
